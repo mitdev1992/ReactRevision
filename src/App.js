ed206f98ecfe7b26bfad5cc7bfa7234068d84beb
MitDev <mitdev211092@gmail.com>
2020-10-28 15:43:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Name Is mit</h1>
+      <p>This course is designed to do React Crash revision.</p>
+    </div>
+  );
+}
+
+export default App;
